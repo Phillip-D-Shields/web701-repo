d590c1525a4fe455ea7063c3dcee78d184f329cd
phillip shields <28024295+Phillip-D-Shields@users.noreply.github.com>
2021-06-01 13:59:31 +1200

    again

--- app/client/src/services/EventService.js
+++ app/client/src/services/EventService.js
@@ -2,11 +2,11 @@ import axios from "axios"
 
 export default {
   async getEvents() {
-    let res = await axios.get("https://web7-backend.herokuapp.com/events");
+    let res = await axios.get("https://web7-backend.herokuapp.com/data/events");
     return res.data;
   },
   async getEventSingle(eventId, accessToken) {
-    let res = await axios.get("https://web7-backend.herokuapp.com/events/" + eventId, {
+    let res = await axios.get("https://web7-backend.herokuapp.com/data/events/" + eventId, {
         headers: {
             Authorization: `Bearer ${accessToken}`
         }
